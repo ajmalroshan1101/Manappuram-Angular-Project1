e5a8af55c0e7dfda1acf11b5c0ebe15999842933
ajmalroshan1101 <ajumalroshan17@gmail.com>
2024-04-10 12:58:07 +0530

    sales report

--- src/app/modules/shared/components/salesreport/salesreport.component.ts
+++ src/app/modules/shared/components/salesreport/salesreport.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ExcelService } from 'src/app/service/excel.service';
 
 @Component({
   selector: 'app-salesreport',
@@ -6,5 +7,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./salesreport.component.css']
 })
 export class SalesreportComponent {
+
+
+  tableData = [
+    "Bill NO",
+    "Trans Date",
+    "Party Name",
+    "Gr Wt",
+    "Cls Wt",
+    "Net Wt",
+    "Mtl Amt",
+    "Gold Rate",
+    "Karat",
+    "Labour Value",
+    "Ggst Amt",
+    "Sgst Amt",
+    "IgstAmt",
+    "Tcs Amy",
+    "Final Amount",
+    "Purchase MC",
+    "Branch Name",
+    "State",
+    "Sub Category",
+    "Product Style",
+    "Inward Party Name",
+    "Category",
+    "Mobilizer"
+  ];
+  
+  startDate!: string;
+  endDate!: string;
+
+  show:boolean= true;
+  
+  constructor(private excelService: ExcelService) { }
+
+
+  exportTableToExcel(): void {
+    this.excelService.exportToExcel(this.tableData);
+  }
+
+  submitDates() {
+    console.log('Start Date:', this.startDate);
+    console.log('End Date:', this.endDate);
+    // You can perform further actions here, such as sending the dates to a service
+
+    this.show= false;
+  }
 
 }
